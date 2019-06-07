7e15f2e7dddb9cd1b944658bcddd06762c37f476
CoderBear801 <coderxiong@gmail.com>
2019-06-07 10:49:16 -0700

    Use MLD instead of Contraction for route calculation and data generation

--- docker-orchestration/osrm-build-routed-docker/docker-entrypoint.sh
+++ docker-orchestration/osrm-build-routed-docker/docker-entrypoint.sh
@@ -13,10 +13,10 @@ if [ "$1" = 'osrm' ]; then
       curl $3 > $DATA_PATH/$2.osm.pbf
     fi
     ./osrm-extract $DATA_PATH/$2.osm.pbf -p profile.lua
-    ./osrm-contract $DATA_PATH/$2.osrm
+    ./osrm-partition $DATA_PATH/$2.osrm
   fi
 
-  ./osrm-routed $DATA_PATH/$2.osrm --max-table-size 8000 &
+  ./osrm-routed $DATA_PATH/$2.osrm -a MLD --max-table-size 8000 &
   child=$!
   wait "$child"
 else
