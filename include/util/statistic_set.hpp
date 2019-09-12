9e452f98c0f3a6eb3492eff9446c968b035ecd79
CoderBear801 <coderxiong@gmail.com>
2019-09-12 16:19:20 -0700

    feat: Add class for statistic set and related unit test issue: #49

---
+++ include/util/statistic_set.hpp
@@ -0,0 +1,109 @@
+#ifndef OSRM_UTIL_STATISTIC_SET_HPP
+#define OSRM_UTIL_STATISTIC_SET_HPP
+
+#include "tbb/concurrent_unordered_set.h"
+
+#include <set>
+#include <string>
+#include <sstream>
+#include <iomanip>
+
+
+namespace osrm
+{
+namespace util
+{
+
+template<typename T>
+class StatisticSet
+{
+// https://www.threadingbuildingblocks.org/docs/help/reference/containers_overview/concurrent_unordered_set_cls.html
+using SET = tbb::concurrent_unordered_set<T>;
+using ITERATOR = typename tbb::concurrent_unordered_set<T>::iterator;
+using CONST_ITERATOR = typename tbb::concurrent_unordered_set<T>::const_iterator;
+
+public:
+    StatisticSet(bool b = false)
+    : enabled(b)
+    {
+        totalnum = -1;
+    }
+
+    void SetTotalNum(const int64_t num)
+    {
+        if (num <= 0)
+        {
+            return;
+        }
+        totalnum = num;
+    }
+
+    void Add(const T n)
+    {
+        if (enabled)
+        {
+           s.insert(n);
+        }
+     }
+
+    void Clear()
+    {
+        if (enabled)
+        {
+            s.clear();
+        }
+        
+    }
+
+    bool Check(const T n) const
+    {
+        if (enabled)
+        {
+            return s.find(n) != s.end();
+        }
+        else
+        {
+            return false;
+        }
+    } 
+
+    bool IsEnabled() const
+    {
+        return enabled;
+    }
+
+    std::string Statistic()
+    {
+        std::ostringstream ss;
+
+        ss << "There are " << s.size() << " nodes be updated.  ";
+        if (totalnum > 0)
+        {
+            float percentage = (float)(s.size() * 100) / totalnum;
+            ss <<std::setprecision(4)<< "About " << percentage << "% of " << totalnum << " elements.\n";
+        }
+
+        return ss.str();
+    }
+
+    CONST_ITERATOR begin() const
+    {
+        return s.begin();
+    }
+
+    CONST_ITERATOR end() const
+    {
+        return s.end();
+    }
+
+private:
+    SET         s;
+    int64_t     totalnum;
+    bool        enabled;
+};
+
+}
+
+}
+
+#endif
---
+++ unit_tests/util/statistic_set.cpp
@@ -0,0 +1,108 @@
+#include "util/statistic_set.hpp"
+#include "util/typedefs.hpp"
+
+#include <boost/test/unit_test.hpp>
+#include <set>
+
+BOOST_AUTO_TEST_SUITE(node_set_test)
+
+using namespace osrm;
+using namespace osrm::util;
+
+BOOST_AUTO_TEST_CASE(statistic_set_enable_func_test)
+{
+    StatisticSet<NodeID> ns1(false);
+    BOOST_CHECK(!ns1.IsEnabled());
+
+    StatisticSet<NodeID> ns2(true);
+    BOOST_CHECK(ns2.IsEnabled());
+}
+
+BOOST_AUTO_TEST_CASE(node_set_disable_test)
+{
+    StatisticSet<NodeID> ns;
+    BOOST_CHECK(!ns.IsEnabled());
+
+    ns.Add(1);
+    BOOST_CHECK(!ns.Check(1));
+    BOOST_CHECK(!ns.Check(2));
+}
+
+BOOST_AUTO_TEST_CASE(node_set_enable_test)
+{
+    StatisticSet<NodeID> ns(true);
+    BOOST_CHECK(ns.IsEnabled());
+
+    ns.Add(1);
+    BOOST_CHECK(ns.Check(1));
+
+    BOOST_CHECK(!ns.Check(2));
+    ns.Add(2);
+    BOOST_CHECK(ns.Check(2));
+}
+
+BOOST_AUTO_TEST_CASE(node_set_percentage_test_empty_set)
+{
+    // Empty set, no TotalNum set
+    StatisticSet<NodeID> ns1(true);
+    std::string str1 = ns1.Statistic();
+    BOOST_CHECK_EQUAL(str1, "There are 0 nodes be updated.  ");
+}
+
+BOOST_AUTO_TEST_CASE(node_set_percentage_test_none_empty_set)
+{
+    // None-empty set, no TotalNum set
+    StatisticSet<NodeID> ns1(true);
+    ns1.Add(1);
+    ns1.Add(1);
+    ns1.Add(2);
+    std::string str1 = ns1.Statistic();
+    BOOST_CHECK_EQUAL(str1, "There are 2 nodes be updated.  ");
+}
+
+BOOST_AUTO_TEST_CASE(node_set_percentage_test_totalnum_statistic)
+{
+    // None-empty set, with TotalNum set
+    StatisticSet<NodeID> ns1(true);
+
+    for (int i = 0; i < 23; ++i)
+    {
+        ns1.Add(i);
+    }
+    ns1.SetTotalNum(100);
+    std::string str = ns1.Statistic();
+    BOOST_CHECK_EQUAL(str, "There are 23 nodes be updated.  About 23% of 100 elements.\n");
+}
+
+BOOST_AUTO_TEST_CASE(node_set_percentage_test_rounding)
+{
+    // None-empty set, with TotalNum set
+    StatisticSet<NodeID> ns1(true);
+
+    for (int i = 0; i < 37; ++i)
+    {
+        ns1.Add(i);
+    }
+    ns1.SetTotalNum(84);
+    std::string str = ns1.Statistic();
+    BOOST_CHECK_EQUAL(str, "There are 37 nodes be updated.  About 44.05% of 84 elements.\n");
+}
+
+BOOST_AUTO_TEST_CASE(node_set_iteration)
+{
+    StatisticSet<NodeID> ns1(true);
+    for (int i = 0; i < 10; ++i)
+    {
+        ns1.Add(i);
+    }
+
+    //int expect = 0;
+    std::set<NodeID> s;
+    for (auto iter : ns1)
+    {
+        s.insert(iter);
+    }
+    BOOST_CHECK_EQUAL(s, 10);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
