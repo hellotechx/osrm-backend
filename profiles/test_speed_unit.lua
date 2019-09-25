585bb3b55e0f28da86a5b2c9cd64c792fc5b2cc8
Yang, Chen <chyang@telenav.cn>
2019-09-25 14:48:12 +0800

    Take speed unit as input in speed parser

--- profiles/test_speed_unit.lua
+++ profiles/test_speed_unit.lua
@@ -1,7 +1,8 @@
-print('hello world')
 
 local tags = require('lib/tags')
+local Measure = require('lib/measure')
 
+-- Test get speed uint by key
 local way = {
     speed_unit = 'M'
 }
@@ -11,12 +12,47 @@ function way:get_value_by_key(k)
 end
 
 local speed_unit = tags.get_speed_unit_by_key(way, 'speed_unit')
+print(speed_unit)
 
--- If speed_unit == 'M', then it's mile per hour
--- If speed_unit == 'K', then it's kilometer per hour
-local adjust_speed_by_unit = 1
-if speed_unit == 'M' then
-    adjust_speed_by_unit = 1.60934
-end
+-- Test cases:
+-- 1. speed without unit + speed unit in mile per hour
+local source = "50"
+speed_unit = "M"
 
-print(speed_unit, adjust_speed_by_unit)
+local n = Measure.get_max_speed(source, speed_unit)
+print(n)
+
+-- 2. speed without unit + speed unit in kilometer per hour
+source = "50"
+speed_unit = "K"
+
+n = Measure.get_max_speed(source, speed_unit)
+print(n)
+
+-- 3. speed with unit + speed unit in mile per hour
+source = "50 mph"
+speed_unit = "M"
+
+n = Measure.get_max_speed(source, speed_unit)
+print(n)
+
+-- 4. speed with unit + speed unit in kilometer per hour
+source = "50 mph"
+speed_unit = "K"
+
+n = Measure.get_max_speed(source, speed_unit)
+print(n)
+
+-- 5. speed with unit + speed unit in kilometer per hour
+source = "50 kph"
+speed_unit = "K"
+
+n = Measure.get_max_speed(source, speed_unit)
+print(n)
+
+-- 6. speed with unit + speed unit in mile per hour
+source = "50 kph"
+speed_unit = "M"
+
+n = Measure.get_max_speed(source, speed_unit)
+print(n)
